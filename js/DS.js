ae97fb18e2ed5d864ae2376e097f4e826d680981
KaueSG <kaue.gonzaga@escola.pr.gov.br>
2025-08-26 08:48:39 -0300

    Finalizando o JS

--- js/DS.js
+++ js/DS.js
@@ -5,8 +5,15 @@ document.getElementById('adicionar').onclick = function(){
 
     if(amigo){
         amigos.push(amigo)
+        document.getElementById('nome').value = ''
+        document.getElementById('lista').innerHTML = ''
         for(let i = 0; i < amigos.length; i++){
-            document.getElementById('lista').innerHTML = `${amigos[i]}`
+            document.getElementById('lista').innerHTML += `<li>${amigos[i]}`
         }
     }
+}
+
+document.getElementById('sortear').onclick = function(){
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)]
+    document.getElementById('lista2').innerHTML = `<li>O sorteado é: ${sorteado}</li>`
 }
